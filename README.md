652031301f76974f826651551f6f8655b1db66ee
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2021-08-23 10:56:35 +0900

    made basic model and schema, added logo

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # virtual_shop_backend
 This is the backend of a virtual shop project.
 
+[the website of this project]()
 [the frontend of this project]()
 
 ## techs used
+<details open>
+<summary>open</summary>
+    <img src="./public/Python_logo.svg" alt="Python" width="70" height="70">
+    <img src="./public/Django_logo.svg" alt="Django" width="100">
+    <img src="./public/Graphene-django_logo.svg" alt="Django" width="70"  height="70">
+
+
+
+</details>
+
+## technical background
+
+-  for database with more complex relationships, I will probably use UML in designing the DB.
 
--- shop/models.py
+++ shop/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 
 
-def Product(Model):
-    name = models.CharField(max_length=200)
+class Product(models.Model):
+    #id
+    name = models.CharField(max_length=128)
     price = models.IntegerField()
-    #image = 
+    image = models.ImageField()
+
     short_description = models.CharField(max_length=64)
-    long_description = models.CharField(max_length=512)
-    unit_sold = models.IntegerField(default=0)
-    date_published = models.DateTimeField("date published")
+    long_description = models.CharField(max_length=1024)
+    
+    unit_sold = models.IntegerField(blank=True, default=0)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
