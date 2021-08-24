9a65c2b46a31b5b40dd8b4b562c9b02f805a01c2
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2021-08-24 23:33:08 +0900

    functioning GraphQL mutation

--- GraphQL/schema.py
+++ GraphQL/schema.py
@@ -3,20 +3,39 @@ from graphene_django import DjangoObjectType
 
 from shop.models import Product
 
+#queries
+
 class ProductType(DjangoObjectType):
     class Meta:
         model = Product
         fields = "__all__"
 
+#mutations
+
+class UpdateProduct(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        change = graphene.Int(required=True)
+
+    product = graphene.Field(ProductType)
+
+    @classmethod
+    def mutate(cls, root, into, id, change):
+        product = Product.objects.get(pk=id)
+        product.unit_sold += change
+        product.save()
+        return UpdateProduct(product=product)
+
+
+    
 
 
 
-
-
+#query
 
 class Query(graphene.ObjectType):
     all_products = graphene.List(ProductType)
-    product_by_id = graphene.Field(ProductType, id=graphene.String())
+    product_by_id = graphene.Field(ProductType, id=graphene.Int(required=True))
 
     def resolve_all_products(root, info):
         return Product.objects.all()
@@ -24,9 +43,13 @@ class Query(graphene.ObjectType):
     
     def resolve_product_by_id(root, info, id):
         return Product.objects.get(pk=id)
+        #return null if product with that id does not exist
 
+#mutation
 
+class Mutation(graphene.ObjectType):
+    update_product = UpdateProduct.Field()
 
+#schema
 
-
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- shop/admin.py
+++ shop/admin.py
@@ -22,7 +22,7 @@ class ProductModelForm(forms.ModelForm):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "short_description", "price", "unit_sold", "date_created", "last_modified", "income")
     list_filter = ("date_created", "last_modified")
-    search_fields = ("name",)
+    search_fields = ("name", "short_description", "long_description")
     #ordering = ("last_modified",)      // ordering is the active table order by default
     date_hierarchy = "last_modified"
     form = ProductModelForm
